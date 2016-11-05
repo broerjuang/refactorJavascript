c7dff5f8ba51ed90cf55d70e7a42ce16e333e8ba
broerjuang <juangwiantoro@gmail.com>
2016-11-05 21:10:57 +0700

    refactor the code using es6 and oop pradigm

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 'use strict'
-$('document').ready(() => {
-  $('#roller button.add').on('click', () => {
-    console.log('WAT');
-    $('.dice').append('<div class="die">0</div>');
-  })
-  $('#roller button.roll').on('click', () => {
-    $('.die').each((k, die) => {
-      var value = Math.floor((Math.random() * 6) + 1)
-      $(die).text(value)
-    })
-  })
+
+const model = {
+  diceNumber : (k, die) => {
+    let value = Math.floor((Math.random() * 6) + 1)
+    $(die).text(value)
+  }
+}
+
+const view = {
+  newDice : () => {
+    $('.dice').append('<div class="die">0</div>')
+  }
+}
+
+const controller = {
+  rollingDice : () => {
+    $('.die').each(model.diceNumber)
+  }
+}
+
+$(document).ready(() => {
+    $('#roller button.add').on('click', view.newDice)
+    $('#roller button.roll').on('click', controller.rollingDice)
 })
